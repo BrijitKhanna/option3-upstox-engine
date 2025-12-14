d22405efc1f9481b840fd484e9f3da81a86c85f6
Brijit Khanna <brijit.khanna@gmail.com>
2025-12-14 20:51:19 +0530

    Debug startup env var loading

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+import os
 from fastapi import FastAPI
 from app.webhook.tradingview import router as tradingview_router
 
+print("BOOTING APP...")
+print("ENV TV_WEBHOOK_SECRET =", os.getenv("TV_WEBHOOK_SECRET"))
+
 app = FastAPI()
 
-# IMPORTANT: prefix = /webhook
 app.include_router(tradingview_router, prefix="/webhook")
 
 @app.get("/")
